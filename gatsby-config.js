28f025384b3d4857fe44f96e2940e322555a651e
Zhijiang Li <zl256@cornell.edu>
2022-07-09 00:07:42 -0700

    Fix headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,7 +142,7 @@ module.exports = {
       options: {
         allPageHeaders: [
           'Referrer-Policy: strict-origin-when-cross-origin',
-          'X-Content-Type-Options = nosniff',
+          'X-Content-Type-Options: nosniff',
           'X-Frame-Options: DENY',
           'X-XSS-Protection: 1; mode=block',
           "Content-Security-Policy: frame-ancestors 'none';",
